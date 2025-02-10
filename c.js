035eec7e4cb485dfa8b8d7ce12bd5842d8d25803
Ali-GH <alighanbari8555@gmail.com>
2025-02-11 00:04:46 +0330

    -

---
+++ c.js
@@ -0,0 +1,34 @@
+const jsonData = [
+  {
+    "category": "Python",
+    "time":"week 1",
+    "items": [
+      "Installing IDE",
+      "Data: Types, Values, Variables, and Names, Input",
+      "Numbers",
+      "Text Strings",
+      "Using pip"
+    ]
+  },
+  {
+    "category": "JavaScript",
+    "time":"week 2",
+    "items": [
+      "Setting up Environment",
+      "Variables and Data Types",
+      "Functions and Scope",
+      "DOM Manipulation"
+    ]
+  },
+  {
+    "category": "JavaScript",
+    "time":"week 3",
+    "items": [
+      "Setting up Environment",
+      "Variables and Data Types",
+      "Functions and Scope",
+      "DOM Manipulation"
+    ]
+  },
+
+]
