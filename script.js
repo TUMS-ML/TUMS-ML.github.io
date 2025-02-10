d558d4526d83649b24107de75dfdbfbacbb9cd49
Ali-GH <alighanbari8555@gmail.com>
2025-02-10 17:04:56 +0330

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".navbar-st a");
+
+    function setActiveLink() {
+        let scrollPosition = window.scrollY;
+
+        sections.forEach((section, index) => {
+            const top = section.offsetTop - 60;
+            const bottom = top + section.clientHeight;
+
+            if (scrollPosition >= top && scrollPosition < bottom) {
+                navLinks.forEach(link => link.classList.remove("active"));
+                navLinks[index].classList.add("active");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", setActiveLink);
+});
